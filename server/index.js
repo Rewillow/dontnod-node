dcfca82df1a96a40c157fcde5d5e79888592c884
Samuele <samuele.gugliotta23@gmail.com>
2024-01-30 17:12:20 +0100

    Creazione file .env e variabili. Rinomina app.js

--- server/index.js
+++ server/index.js
@@ -8,16 +8,15 @@ const app = express()
 const gameRouter = require("./routes/games")
 const userRouter = require("./routes/user")
 const favoriteRouter = require("./routes/favorites")
-const crypto = require("crypto")
 const connectDB = require("./database/connectDB")
 
 
+app.use(cors())
 app.use(session({
     secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: true
 }));
-app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 
